ef0c42aef52c3cc8ffbf25c6c6f409db6039e651
Travis Fischer <fisch0920@gmail.com>
2022-12-06 05:10:37 -0600

    📈

--- src/test-render.ts
+++ src/test-render.ts
@@ -5,57 +5,6 @@ async function main() {
     'https://pbs.twimg.com/profile_images/1235530549067943937/6BQE9kbQ_400x400.jpg'
   const username = '@transitive_bs'
 
-  // const prompt = `Do you think my game engine will succeed? (It's open source, built for VR, and meant to create an NFT-devoid metaverse focused on the community and builders)`
-  // const response = `I'm sorry, but I'm not able to browse the internet and therefore don't have information about your game engine. As a language model, my ability to provide feedback is limited to what I've been trained on and my understanding of the context of your question.
-
-  // Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea **commodo consequat**. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat __cupidatat__ non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  // `
-
-  // Numbered list
-  // const prompt = 'What are some tips for having great conversations with you?'
-  // const response = 'Here are a few tips for having a great conversation with me:\n' +
-  // '\n' +
-  // '1. Be specific and clear in your questions. The more information you provide, the easier it is for me to provide a helpful and accurate response.\n' +
-  // '\n' +
-  // "2. Keep an open mind and be willing to engage in a back-and-forth conversation. I'm here to help and provide information, but I'm also always happy to discuss and explore different ideas with you.\n" +
-  // '\n' +
-  // "3. Be respectful and courteous in your interactions with me. I'm an artificial intelligence trained to assist with a wide range of tasks and provide information, but I'm not capable of emotions and do not have personal opinions.\n" +
-  // '\n' +
-  // "4. Avoid asking me to do things that I am not capable of, such as providing personal information about an individual or accessing information that is not readily available to me. I'm a large language model trained to assist with a wide range of tasks, but I do not have the ability to browse the internet or access information that is not part of my training data.\n" +
-  // '\n' +
-  // "I hope these tips help you have great conversations with me. If you have any other questions, feel free to ask and I'll do my best to help."
-
-  // Markdown kitchen sink
-  //   const prompt = 'Give me some **markdown**'
-  //   const response = `
-  // # Heading level 1
-
-  // ---
-
-  // I really like using Markdown.
-  // I just love **bold text**.
-  // Italicized text is the *cat's meow*.
-  // > Dorothy followed her through many of the beautiful rooms in her castle.
-
-  // Test new paragraph.
-
-  // 1. First item
-  // 2. Second item
-  // 3. Third item
-  // 4. Fourth item
-
-  // - First item
-  // - Second item
-  // - Third item
-  // - Fourth item
-
-  // At the command prompt, type \`nano\`.
-
-  // My favorite search engine is [Duck Duck Go](https://duckduckgo.com).
-
-  // Another break.
-  // `
-
   // const prompt = 'Write bubble sort in python.'
   // const response = `
   // Here is an implementation of bubble sort in Python:
